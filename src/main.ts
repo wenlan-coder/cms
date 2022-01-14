3595e5606418e58967de031686add619f082ec6e
wenlan12 <552109783@qq.com>
2022-01-14 22:43:26 +0800

    test

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: wenlan
+ * @Date: 2022-01-13 15:26:43
+ * @LastEditors: wenlan
+ * @LastEditTime: 2022-01-14 22:42:12
+ */
+import { createApp, ssrContextKey } from 'vue'
+import App from "./App.vue"
+console.log(a);
+
+
+createApp(App).mount('#app')
