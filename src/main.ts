b78d733ab3f7a719796bca07f144a07a8a5cf431
wenlan12 <552109783@qq.com>
2022-01-15 14:45:04 +0800

    test02

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@
  * @Author: wenlan
  * @Date: 2022-01-13 15:26:43
  * @LastEditors: wenlan
- * @LastEditTime: 2022-01-15 14:32:35
+ * @LastEditTime: 2022-01-15 14:44:44
  */
-import { createApp } from "vue"
+import { createApp } from "vue";
+
 import App from './App.vue';
 
 
@@ -17,4 +18,4 @@ import App from './App.vue';
 
 
 
-createApp(App).mount('#app');
+createApp(App).mount('#app')
