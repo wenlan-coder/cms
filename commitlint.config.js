90185e28f2c1f9a264524fe0b27e5d5c8058330e
wenlan12 <552109783@qq.com>
2022-01-15 15:21:46 +0800

    style(login): test commit login

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: wenlan
+ * @Date: 2022-01-15 15:12:21
+ * @LastEditors: wenlan
+ * @LastEditTime: 2022-01-15 15:12:23
+ */
+module.exports = { extends: ['@commitlint/config-conventional'] }
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,14 @@
  * @Author: wenlan
  * @Date: 2022-01-13 15:26:43
  * @LastEditors: wenlan
- * @LastEditTime: 2022-01-15 14:44:44
+ * @LastEditTime: 2022-01-15 15:19:26
  */
 import { createApp } from 'vue'
 
-import App from './App.vue'
+import App from './App.vue';
+
+
+
+
 
 createApp(App).mount('#app')
